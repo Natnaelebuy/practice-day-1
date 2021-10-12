ea2acab5ed097624534a29290e1d6f3863cfa242
Natnael <natnaelebuy@gmail.com>
2021-10-11 17:14:21 -0700

    update birthday

---
+++ Birthday.java
@@ -0,0 +1,28 @@
+import java.util.HashMap;
+
+public class Birthday {
+   
+    public static void main(String[] args) {
+        HashMap<String, String> birthdays = new HashMap<>();
+
+        birthdays.put("Hawaz", "07/12/1997");
+        birthdays.put("Rahul", "11/01/1998");
+        
+
+
+
+        
+
+/// let's creat float in to HashMap
+//HashMap<Float, Integer> map = new HashMap<Float, Integer>();
+
+
+
+
+
+
+
+    }
+
+    
+}
